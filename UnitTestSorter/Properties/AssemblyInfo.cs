725154c5716b83f93acedad1f1741e86ffa4f46e
anton <anton@DESKTOP-KKBE2RB>
2021-03-12 13:50:19 +0700

    added UnitTest

---
+++ UnitTestSorter/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestSorter")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestSorter")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("5a87d79f-e73e-4402-b998-05579314001f")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTestSorter/UnitTest1.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using UnitTestSorter;
+
+namespace UnitTestSorter
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void SortMethods()
+        {
+            Sorter.MainWindow mw = new Sorter.MainWindow();
+            CollectionAssert.AreEqual(mw.sort_five(new int[5] { 1, 3, 2, 1, 4 }), new int[5] { 1, 1, 2, 3, 4 });
+            CollectionAssert.AreEqual(mw.sort_five(new int[5] { 1, 3, 2, 1, 4 }), new int[5] { 1, 1, 2, 3, 4 });
+            CollectionAssert.AreEqual(mw.sort_five(new int[5] { 1, 3, 2, 1, 4 }), new int[5] { 1, 1, 2, 3, 4 });
+        }
+    }
+}
